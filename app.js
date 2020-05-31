bc835e6635ac4f02c2f56abac2363718968f954c
Park Jack <parker.jackson.hhs@gmail.com>
2020-05-30 20:30:42 -0400

    installed and required packages for sessions and cookies

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+const session = require('express-session'); 
+const cookieParser = require('cookie-parser')
+
+
 const PORT = process.env.PORT || 3000;
 
 const userRouter = require('./routes/users');
@@ -9,12 +13,26 @@ const subsedditRouter = require('./routes/subseddits');
 const postRouter = require('./routes/posts');
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', 'views'); 
+
+app.use(cookieParser()); 
+app.use(session({ 
+  secret:'secret', 
+  resave:false, 
+  saveUninitialized: true, 
+  cookie: { 
+    secure:false, 
+    maxAge: 2592000
+  }
+
+
+}))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+app.get
 
 app.get("/home",(req,res)=> { 
   res.render("index", { 
